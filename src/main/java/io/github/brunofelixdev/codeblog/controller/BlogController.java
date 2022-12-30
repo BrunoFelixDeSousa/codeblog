dcc8d6da8328eda1dcee57bb25003a063620597f
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-12-30 05:23:27 -0300

    quinta etapa: Criando um método GET com @PathVariable id

--- src/main/java/io/github/brunofelixdev/codeblog/controller/BlogController.java
+++ src/main/java/io/github/brunofelixdev/codeblog/controller/BlogController.java
@@ -5,6 +5,7 @@ import io.github.brunofelixdev.codeblog.service.BlogService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
@@ -20,6 +21,14 @@ public class BlogController {
         ModelAndView modelAndView = new ModelAndView("posts");
         List<Post> posts = blogService.buscarTodosPosts();
         modelAndView.addObject("posts", posts);
+        return modelAndView;
+    }
+
+    @GetMapping("/posts/{id}")
+    public ModelAndView getPostsDetails(@PathVariable("id") Long id) {
+        ModelAndView modelAndView = new ModelAndView("postDetails");
+        Post post = blogService.buscarPostPorId(id);
+        modelAndView.addObject("post", post);
         return modelAndView;
     }
 }
