3ff20b97ed43eef4c97c1cd63ee7b98a792d280b
brunofelixdesousa <brunofelixdesousa@hotmail.com>
2022-12-30 07:12:26 -0300

    setima etapa: Salvando um novo Post

--- src/main/java/io/github/brunofelixdev/codeblog/controller/BlogController.java
+++ src/main/java/io/github/brunofelixdev/codeblog/controller/BlogController.java
@@ -2,12 +2,17 @@ package io.github.brunofelixdev.codeblog.controller;
 
 import io.github.brunofelixdev.codeblog.model.Post;
 import io.github.brunofelixdev.codeblog.service.BlogService;
+import jakarta.validation.Valid;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.validation.BindingResult;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.support.RedirectAttributes;
 
+import java.time.LocalDate;
 import java.util.List;
 
 @Controller
@@ -30,5 +35,21 @@ public class BlogController {
         Post post = blogService.buscarPostPorId(id);
         modelAndView.addObject("post", post);
         return modelAndView;
+    }
+
+    @GetMapping("/newpost")
+    public String getPostForm() {
+        return "postForm";
+    }
+
+    @PostMapping("/newpost")
+    public String savePost(@Valid Post post, BindingResult result, RedirectAttributes attributes)  {
+        if(result.hasErrors()) {
+            attributes.addFlashAttribute("mensagem", "Verifique se os campos obrigatórios foram preenchidos!");
+            return "redirect:/newpost";
+        }
+        post.setData(LocalDate.now());
+        blogService.salvarPost(post);
+        return "redirect:/posts";
     }
 }
